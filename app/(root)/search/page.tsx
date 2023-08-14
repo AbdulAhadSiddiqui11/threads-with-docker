ca80af472100d1dd57bda5ac7ddec4413ec0258e
Abdul Ahad Siddiqui <abdulahadsiddiqui011@gmail.com>
2023-08-15 01:35:21 +0530

    Search page, functionality and components

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,7 +1,55 @@
+import { fetchUser, fetchUsers } from "@/lib/actions/user.action";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import UserCard from "@/components/cards/UserCard";
+
 const SearchPage = async () => {
+
+    const user = await currentUser();
+
+    if (!user) {
+        return null;
+    }
+
+    const userInfo = await fetchUser(user.id);
+
+    if (!userInfo?.onboarded) {
+        redirect("/onboarding");
+    }
+
+    // fetch users
+    const result = await fetchUsers({
+        userId: user.id,
+        searchString: '',
+        pageNumber: 1,
+        pageSize: 25,
+    });
+
     return (
         <section>
             <h1 className="head-text mb-10">Search</h1>
+
+            {/* search bar */}
+
+            <div className="mt-14 flex flex-col gap-9">
+                {result.users.length === 0 ? (
+                    <p className="no-result">No users</p>
+                ) : (
+                    <>
+                        {result.users.map((user) => (
+                            <UserCard 
+                                key={user.id}
+                                id={user.id}
+                                name={user.name}
+                                username={user.username}
+                                imgUrl={user.image}
+                                personType='User'
+                            />
+                        ))}
+                    </>
+                )}
+            </div>
         </section>
     );
 }
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,6 +5,8 @@ import User from "@/lib/models/user.model";
 import { connectToDatabase } from "../mongoose";
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
+import { skip } from "node:test";
 
 interface UserActionParams {
     userId: string;
@@ -78,5 +80,59 @@ export async function fetchUserThreads(userId: string) {
         return threads;
     } catch (error: any) {
         throw new Error(`Error fetching user threads: ${error.message}`);
+    }
+}
+
+
+interface FetchUserParams {
+    userId: string;
+    searchString?: string;
+    pageNumber?: number;
+    pageSize?: number;
+    sortBy?: SortOrder;
+}
+
+export async function fetchUsers({
+    userId,
+    searchString = '',
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = 'desc',
+}: FetchUserParams) {
+    try {
+        connectToDatabase();
+
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        const regex = new RegExp(searchString, 'i');
+
+        const query: FilterQuery<typeof User> = {
+            id: { $ne: userId },
+        };
+
+        if (searchString.trim() !== '') {
+            query.$or = [
+                { username: { $regex: regex } },
+                { name: { $regex: regex } },
+            ];
+        }
+
+        const sortOptions = { createdAt: sortBy };
+
+        const usersQuery = User.find(query)
+                                .sort(sortOptions)
+                                .skip(skipAmount)
+                                .limit(pageSize);
+
+        const totalUserCount = await User.countDocuments(query);
+
+        const users = await usersQuery.exec();
+
+        const isNext = totalUserCount > skipAmount + users.length;
+
+        return { users, isNext };
+
+    } catch (error: any) {
+        throw new Error(`Error fetching users: ${error.message}`);
     }
 }
