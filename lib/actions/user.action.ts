add12045d24869a54cba6dd9fa352eec5fd1cfe1
Abdul Ahad Siddiqui <abdulahadsiddiqui011@gmail.com>
2023-08-15 02:21:09 +0530

    Activity page updates + backend get activity action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -136,3 +136,27 @@ export async function fetchUsers({
         throw new Error(`Error fetching users: ${error.message}`);
     }
 }
+
+export async function getActivity(userId: string) {
+    try {
+        connectToDatabase();
+
+        // find all threads created by the user
+        const userThreads = await Thread.find({ author: userId });
+
+        // collect all child thread ids (replies) from the children field
+        const childThreadIds = userThreads.reduce((acc, thread) => {
+            return acc.concat(thread.children);
+        }, []);
+
+        const replies = await Thread.find({ _id: { $in: childThreadIds }, author: { $ne: userId } })
+                                        .populate({
+                                            path: 'author',
+                                            model: User,
+                                            select: 'name image _id',
+                                        });
+        return replies;
+    } catch (error: any) {
+        throw new Error(`Error fetching user activity: ${error.message}`);
+    }
+}
