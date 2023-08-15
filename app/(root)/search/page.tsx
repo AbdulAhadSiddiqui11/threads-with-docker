f8d4d7a3d964c414a86034111695c409840b5c82
Abdul Ahad Siddiqui <abdulahadsiddiqui011@gmail.com>
2023-08-15 05:43:28 +0530

    Search and pagination functions

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -3,8 +3,14 @@ import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import Image from "next/image";
 import UserCard from "@/components/cards/UserCard";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
 
-const SearchPage = async () => {
+const SearchPage = async ({
+    searchParams,
+  }: {
+    searchParams: { [key: string]: string | undefined };
+  }) => {
 
     const user = await currentUser();
 
@@ -21,8 +27,8 @@ const SearchPage = async () => {
     // fetch users
     const result = await fetchUsers({
         userId: user.id,
-        searchString: '',
-        pageNumber: 1,
+        searchString: searchParams.q,
+        pageNumber: searchParams?.page ? +searchParams.page : 1,
         pageSize: 25,
     });
 
@@ -30,7 +36,7 @@ const SearchPage = async () => {
         <section>
             <h1 className="head-text mb-10">Search</h1>
 
-            {/* search bar */}
+            <Searchbar routeType='search' />
 
             <div className="mt-14 flex flex-col gap-9">
                 {result.users.length === 0 ? (
@@ -50,6 +56,12 @@ const SearchPage = async () => {
                     </>
                 )}
             </div>
+
+            <Pagination
+                path='search'
+                pageNumber={searchParams?.page ? +searchParams.page : 1}
+                isNext={result.isNext}
+            />
         </section>
     );
 }
